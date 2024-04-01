3a858e4fb1d316ce748ce0d45760793db2888a32
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-04-02 00:27:37 +0900

    급식 정보 Model 설계, JSON 파일로부터 데이터베이스 동기화

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from articles.forms import LoginForm
+import json
 
 # Create your views here.
 def login(request):
@@ -16,3 +17,11 @@ def login(request):
         'form' : form,
     }
     return render(request,'articles/index.html',context)
+
+def menu(request):
+    with open('accounts/static/data.json','r',encoding='utf-8') as f:
+        json_data = json.load(f)
+    context = {
+        'date' : json_data['date'],
+        'menu' : json_data['menu']
+    }
--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class MenuList(models.Model):
+    date = models.DateField()
+    menu_course_type = models.CharField(max_length=2)
+    set_menu_name = models.TextField()
