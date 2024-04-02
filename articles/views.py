f66bf232b28d61673aa520267e3789f3cc4c425f
tjdgus2308 <tjdgus2308@gmail.com>
2024-04-02 15:48:58 +0900

    240402

--- articles/views.py
+++ articles/views.py
@@ -7,32 +7,31 @@ import json
 def index(request):
     with open('accounts/static/data.json','r',encoding='utf-8') as f:
         json_data = json.load(f)
-        for set_menu in json_data['menu']:
-            if MenuList.objects.filter(date=json_data['date'], menu_course_type=set_menu['menu_course_type']):
-                continue
-            else:
+        if not MenuList.objects.filter(date=json_data['date']):
+            for set_menu in json_data['menu']:
                 if set_menu['menu_course_type'] == 'L3':
                     for set in set_menu['set_menu_name']:
                         menu = MenuList(
                             date=json_data['date'],
-                            menu_course_type=set_menu['menu_course_type'],
+                            menu_course_type='10',
                             set_menu_name=set
                             )
                         menu.save()
                 else:
                     menu = MenuList(
                         date=json_data['date'],
-                        menu_course_type=set_menu['menu_course_type'],
+                        menu_course_type='20',
                         set_menu_name=' / '.join(set_menu['set_menu_name'])
                         )
                     menu.save()
-                
+
     form = LoginForm()
-    menus = MenuList.objects.filter(date=json_data['date'])
+    menu_20F = MenuList.objects.filter(date=json_data['date'], menu_course_type='20')
+    menu_10F = MenuList.objects.filter(date=json_data['date'], menu_course_type='10')
     context = {
         'form' : form,
-        '20F_menu' : [menus.get(pk=1),menus.get(pk=2)],
-        '10F_menu' : [menus.get(pk=3), menus.get(pk=4), menus.get(pk=5)]
+        '20F_menu' : menu_20F,
+        '10F_menu' : menu_10F
     }
     return render(request, 'articles/index.html', context)
 
